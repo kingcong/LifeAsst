a93dcec214971323f1f757abb45354c463d9bbec
王聪 <2441413514@qq.com>
2017-02-13 20:41:21 +0800

    首页基本内容修改

--- app/src/main/java/com/ustc/lifeasst/base/impl/HomePager.java
+++ app/src/main/java/com/ustc/lifeasst/base/impl/HomePager.java
@@ -9,6 +9,8 @@ import android.widget.SimpleAdapter;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
+import com.shizhefei.view.indicator.Indicator;
+import com.shizhefei.view.indicator.slidebar.ScrollBar;
 import com.ustc.lifeasst.R;
 import com.ustc.lifeasst.base.BasePager;
 
@@ -37,14 +39,91 @@ public class HomePager extends BasePager {
 	@Override
 	public void initData() {
 		System.out.println("首页初始化啦...");
+		Indicator indicator = new Indicator() {
+			@Override
+			public void setAdapter(IndicatorAdapter indicatorAdapter) {
+
+			}
+
+			@Override
+			public void setOnItemSelectListener(OnItemSelectedListener onItemSelectedListener) {
+
+			}
+
+			@Override
+			public OnItemSelectedListener getOnItemSelectListener() {
+				return null;
+			}
+
+			@Override
+			public void setOnTransitionListener(OnTransitionListener onTransitionListener) {
+
+			}
+
+			@Override
+			public OnTransitionListener getOnTransitionListener() {
+				return null;
+			}
+
+			@Override
+			public void setScrollBar(ScrollBar scrollBar) {
+
+			}
+
+			@Override
+			public IndicatorAdapter getIndicatorAdapter() {
+				return null;
+			}
+
+			@Override
+			public void setCurrentItem(int i) {
+
+			}
+
+			@Override
+			public void setCurrentItem(int i, boolean b) {
+
+			}
+
+			@Override
+			public View getItemView(int i) {
+				return null;
+			}
+
+			@Override
+			public int getCurrentItem() {
+				return 0;
+			}
+
+			@Override
+			public int getPreSelectItem() {
+				return 0;
+			}
+
+			@Override
+			public void onPageScrolled(int i, float v, int i1) {
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int i) {
+
+			}
+
+			@Override
+			public void setItemClickable(boolean b) {
+
+			}
+
+			@Override
+			public boolean isItemClickable() {
+				return false;
+			}
+		};
 
 		View view = View.inflate(mActivity, R.layout.home_pager, null);
-//		ViewUtils.inject(this,view);
-//
 		View mHeaderView = View.inflate(mActivity, R.layout.homepager_listview_header,
 				null);
-//		ViewUtils.inject(this,mHeaderView);
-
 
 		flContent.addView(view);
 
